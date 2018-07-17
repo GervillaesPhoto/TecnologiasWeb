a0fa81183095d7439e9049b1c4d44388b0b3b85e
GervillaesPhoto <gervillaesphoto@gmail.com>
2018-07-17 17:49:57 +0200

    - Setting website for the first time

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="GervillaesPhoto, Fotografía y vídeo en Almería ">
+    <meta name="keywords" content="gervillaesphoto, fotografía, almeria, vídeo">
+    <meta name="author" content="GervillaesPhoto">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>GervillaesPhoto-s</title>
+
+  </head>
+
+  <body>
+        <header class="v-header container">
+          <div class="fullscreen-video-wrap">
+            <video src="video/bghome.mp4" autoplay ="true" loop="true">
+            </video>
+
+         <div class="header-overlay">
+         </div>
+         <div class="header-content">
+           <h1>Gerardo Villagrá Espinar</h1>
+           <h2>Fotógrafo y Filmmaker</h2>
+           <a href="#" class="btn"> Comenzar</a>
+         </div>
+       </header>
+    
+  </body>
+
+</html>
